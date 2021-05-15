48e4dd0a327cbc7cc89410bb3741a061b0045ae0
amiedawn <amie.d.carroll@gmail.com>
2021-05-14 20:46:01 -0600

    finish about and nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Nav() {
+  const categories = [
+    {
+      name: "Cats",
+      description: "cats",
+    },
+    {
+      name: "Dogs",
+      description: "dogs",
+    },
+    {
+      name: "Pandas",
+      description: "pandas",
+    },
+    {
+      name: "Monkeys",
+      description: "monkeys",
+    },
+  ];
+
+  const handleClick = () => {
+    console.log("click handled")
+  }
+
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera"> 🦌</span> Animal Gallery
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about" onClick={() => handleClick()}>
+              About
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick()}>
+              Contact
+            </span>
+          </li>
+          {/* map over the category array to display each category in the Nav  */}
+          {
+            categories.map((category) => (
+            <li
+              className="mx-1"
+              key={category.name}
+            >
+               {/* event listener for every time a category is clicked */}
+              <span onClick={() => { handleClick(); }}>
+                {capitalizeFirstLetter(category.name)} 
+              </span>
+            </li>  
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav;
