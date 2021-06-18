6cf6148851178f986046429ff6eb23f3f83170ff
amiedawn <amie.d.carroll@gmail.com>
2021-06-18 00:58:13 -0600

    add readme

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -29,7 +29,7 @@ function Nav(props) {
 							href="#about"
 							onClick={() => setContactSelected(false)}
 						>
-							About me
+							About
 						</a>
 					</li>
 					<li className={`mx-2 ${contactSelected && "navActive"}`}>
